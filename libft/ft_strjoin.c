26180334a0fb78d12197fbdbaf770bbb8a418d38
hanopav <122623847+hanopav@users.noreply.github.com>
2023-08-30 09:37:48 -0400

    Add files via upload

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phanosek <phanosek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 17:47:07 by phanosek          #+#    #+#             */
+/*   Updated: 2023/01/18 18:10:07 by phanosek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		len;
+
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(sizeof(char) * len);
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s1, len + 1);
+	ft_strlcat(str, s2, len + 1);
+	return (str);
+}
